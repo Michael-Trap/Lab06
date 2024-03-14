f06fa401f1bacb393060b1ffa3d23b9d9d79faef
michael-trap <michaeltrapani@ufl.edu>
2024-03-14 16:57:07 -0400

    Lab 6 initial

--- Lab 6.py
+++ Lab 6.py
@@ -7,28 +7,25 @@ def menu():
     print("2. Decode")
     print("3. Quit")
 
-while True:
-    menu()
-    menu_input = int(input("\nPlease enter an option: "))
+def encode(oldpass):
+    newpass = ""
+    for x in range(len(oldpass)):
+        if int(oldpass[x]) < 7:
+            newpass += str(int(oldpass[x]) + 3)    #offset by + 3
+        else:
+            newpass += str(int(oldpass[x]) + 3 - 10)    #offset by + 3 and remove tens digit
+    return newpass
 
-    if menu_input == 1:
-        password_ = ""
-        password = input("Please enter your password to encode: ")
-        for x in range(len(password)):
-            if int(password[x]) < 7:
-                password_ += str(int(password[x]) + 3)
-            else:
-                password_ += str(int(password[x]) + 3 - 10)
-        #print(password_)
-        print("Your password has been encoded and stored!\n")
+if __name__ == '__main__':
+    while True:
+        menu()
+        menu_input = int(input("\nPlease enter an option: "))
+        if menu_input == 1:
+            password = input("Please enter your password to encode: ")
+            encode(password)
+            print("Your password has been encoded and stored!\n")
+        if menu_input == 2:
+            pass
+        if menu_input == 3:
+            exit()
 
-    if menu_input == 2:
-        pass
-
-    if menu_input == 3:
-        exit()
-
-    else:
-        pass
-
-    # if __name__ == '__main__':
